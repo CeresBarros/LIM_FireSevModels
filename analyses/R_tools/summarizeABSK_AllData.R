c9fd14d1b38efd9a2f1c87cbd2020ce221025481
ecology <ceresvbarros@hotmail.com>
2019-02-25 14:26:51 -0800

    updates to analyses scripts and functions

--- analyses/R_tools/summarizeABSK_AllData.R
+++ analyses/R_tools/summarizeABSK_AllData.R
@@ -1,61 +1,9 @@
-## FUNCTION TO SUMMARIZE DATA
+## FUNCTIONS TO SUMMARIZE DATA
 ## calculates summary statistics on weather data and reshapes species data to species columns containing
-## percent cover values. Ignores understory layer, but creates a column of understory presence/absence
-summarizeABSK_AllData <- function(DT) {
-  summaryDT <- DT %>%
-    group_by(pixID) %>%
-    summarise(startJulDay = min(julDay),
-              meanTemp = mean(temp),
-              minTemp = min(temp),
-              maxTemp = max(temp),
-              cvTemp = sd(temp)/mean(temp),
-              rangeRH = max(rh) - min(rh),
-              meanRH = mean(rh),
-              minRH = min(rh),
-              maxRH = max(rh),
-              cvRH = sd(rh)/mean(rh),
-              rangeRH = max(rh) - min(rh),
-              meanWS = mean(ws),
-              minWS = min(ws),
-              maxWS = max(ws),
-              cvWS = sd(ws)/mean(ws),
-              rangeWS = max(ws) - min(ws),
-              meanRain = mean(rain),
-              minRain = min(rain),
-              maxRain = max(rain),
-              cvRain = sd(rain)/mean(rain),
-              rangeRain = max(rain) - min(rain),
-              meanFFMC = mean(ffmc),
-              minFFMC = min(ffmc),
-              maxFFMC = max(ffmc),
-              cvFFMC = sd(ffmc)/mean(ffmc),
-              rangeFFMC = max(ffmc) - min(ffmc),
-              meanDMC = mean(dmc),
-              minDMC = min(dmc),
-              maxDMC = max(dmc),
-              cvDMC = sd(dmc)/mean(dmc),
-              rangeDMC = max(dmc) - min(dmc),
-              meanDC = mean(dc),
-              minDC = min(dc),
-              maxDC = max(dc),
-              cvDC = sd(dc)/mean(dc),
-              rangeDC = max(dc) - min(dc),
-              meanISI = mean(isi),
-              minISI = min(isi),
-              maxISI = max(isi),
-              cvISI = sd(isi)/mean(isi),
-              rangeISI = max(isi) - min(isi),
-              meanBUI = mean(bui),
-              minBUI = min(bui),
-              maxBUI = max(bui),
-              cvBUI = sd(bui)/mean(bui),
-              rangeBUI = max(bui) - min(bui),
-              meanFWI = mean(fwi),
-              minFWI = min(fwi),
-              maxFWI = max(fwi),
-              cvFWI = sd(fwi)/mean(fwi),
-              rangeFWI = max(fwi) - min(fwi)) %>%
-    data.table(.)
+## percent cover values per pixel. Ignores understory layer, but creates a column of understory presence/absence
+
+summarizeABSK_AllData <- function(DT, saveDir, overwrite = FALSE) {
+  summaryDT <- summarizeClimateVars(DT = DT, saveDir = saveDir, overwrite = overwrite)
   
   ## merge with remaining data
   setkey(summaryDT, pixID)
@@ -85,7 +33,8 @@ summarizeABSK_AllData <- function(DT) {
   
   summaryDT <- melt(summaryDT, measure = list(colA, colB), 
                     value.name = c("SPEC", "SPEC_PER"), variable.name = "SPEC_dominance")
-  rm(colA, colB); amc::.gc()
+  rm(colA, colB)
+  amc::.gc()
   
   ## and now re-cast to have a column per species
   summaryDT <- dcast(summaryDT, ... ~ SPEC, value.var = "SPEC_PER", fill = 0)
@@ -94,8 +43,96 @@ summarizeABSK_AllData <- function(DT) {
   cols <- grep("^NA$", names(summaryDT))
   summaryDT[, (cols) := NULL]
   
-  ## check for duplicates
+  ## remove potential duplicates
   summaryDT <- unique(summaryDT)  
+  
+  ## ignore species dominance variable - not needed as dominance is defined by cover
+  cols <- grep("SPEC.$", names(DT), value = TRUE)
+  species <- unique(unlist(unique(DT[, ..cols])))
+  species <- species[!is.na(species) & species != "NA"]
+  summaryDTsp <- summaryDT[, lapply(.SD, sum), by = "pixID", .SDcols = species]
+  
+  ## check if any has >100 cover - good
+  # summary(summaryDT[, ..species])
+  
+  ### clear memory
+  amc::.gc()
+  
+  ## merge summarised species table with remaining variables
+  setkey(summaryDT, pixID)
+  setkey(summaryDTsp, pixID)
+  cols <- grep(paste0(paste(species, collapse = "|"), "|SPEC_dominance"), names(summaryDT),
+               invert = TRUE, value = TRUE)
+  summaryDT <- summaryDTsp[unique(summaryDT[, ..cols])]
   
   summaryDT
 }
+
+## Function to summarize fire weather columns.
+## Used internally by summarizeABSK_AllData to enable caching
+summarizeClimateVars <- function(DT, saveDir, overwrite = FALSE) {
+  fileName <- file.path(saveDir, "summClimateVarsAllDataABSK.rds")
+  if (!file.exists(fileName) | overwrite) {
+    summaryDT <- DT %>%
+      group_by(pixID) %>%
+      summarise(startJulDay = min(julDay),
+                meanTemp = mean(temp),
+                minTemp = min(temp),
+                maxTemp = max(temp),
+                cvTemp = sd(temp)/mean(temp),
+                rangeRH = max(rh) - min(rh),
+                meanRH = mean(rh),
+                minRH = min(rh),
+                maxRH = max(rh),
+                cvRH = sd(rh)/mean(rh),
+                rangeRH = max(rh) - min(rh),
+                meanWS = mean(ws),
+                minWS = min(ws),
+                maxWS = max(ws),
+                cvWS = sd(ws)/mean(ws),
+                rangeWS = max(ws) - min(ws),
+                meanRain = mean(rain),
+                minRain = min(rain),
+                maxRain = max(rain),
+                cvRain = sd(rain)/mean(rain),
+                rangeRain = max(rain) - min(rain),
+                meanFFMC = mean(ffmc),
+                minFFMC = min(ffmc),
+                maxFFMC = max(ffmc),
+                cvFFMC = sd(ffmc)/mean(ffmc),
+                rangeFFMC = max(ffmc) - min(ffmc),
+                meanDMC = mean(dmc),
+                minDMC = min(dmc),
+                maxDMC = max(dmc),
+                cvDMC = sd(dmc)/mean(dmc),
+                rangeDMC = max(dmc) - min(dmc),
+                meanDC = mean(dc),
+                minDC = min(dc),
+                maxDC = max(dc),
+                cvDC = sd(dc)/mean(dc),
+                rangeDC = max(dc) - min(dc),
+                meanISI = mean(isi),
+                minISI = min(isi),
+                maxISI = max(isi),
+                cvISI = sd(isi)/mean(isi),
+                rangeISI = max(isi) - min(isi),
+                meanBUI = mean(bui),
+                minBUI = min(bui),
+                maxBUI = max(bui),
+                cvBUI = sd(bui)/mean(bui),
+                rangeBUI = max(bui) - min(bui),
+                meanFWI = mean(fwi),
+                minFWI = min(fwi),
+                maxFWI = max(fwi),
+                cvFWI = sd(fwi)/mean(fwi),
+                rangeFWI = max(fwi) - min(fwi)) %>%
+      data.table(.)
+    
+    saveRDS(summaryDT, file = fileName)
+    return(summaryDT)
+  } else {
+    readRDS(filename)
+    return(summaryDT)
+  }
+}
+

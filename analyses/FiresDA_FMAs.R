ba2e4795d160b591ea60bd83ff9515220f0e454e
CeresBarros <cbarros@mail.ubc.ca>
2020-05-06 16:08:06 -0700

    minor

--- analyses/FiresDA_FMAs.R
+++ analyses/FiresDA_FMAs.R
@@ -36,7 +36,7 @@ folder = "~/../OneDrive/Documents/LandscapesInMotion/data/fires_Dave/Projected_r
 # DA_fires <- prepInputs(targetFile = file.path(dataDIR, "albertafires1_postfire.shp"),
 #                        url = "https://drive.google.com/file/d/1wGCqI_X1t-PDM5eO6JWQW9hpNv_8zlum/view?usp=sharing",
 #                        archive = "Projected_renamed.zip",
-#                        alsoExtract = file.path(dataDIR, c("albertafires1_postfire.", "albertafires2_postfire.", 
+#                        alsoExtract = file.path(dataDIR, c("albertafires1_postfire.", "albertafires2_postfire.",
 #                                                           "albertafires1_pretfire.", "albertafires2_prefire.",
 #                                                           "saskatchewanfires_postfire.","saskatchewanfires_prefire.")),
 #                        fun = "st_read", pkg = "sf",
@@ -64,26 +64,26 @@ if(all(saskatchewanfires_postfire$PERIMETER_ == saskatchewanfires_postfire$PERIM
 }
 
 ## Uniformize column names
-names(albertafires2_postfire)[grep("YEAR", names(albertafires2_postfire))] = 
-  names(saskatchewanfires_postfire)[grep("EAR", names(saskatchewanfires_postfire))] = 
+names(albertafires2_postfire)[grep("YEAR", names(albertafires2_postfire))] =
+  names(saskatchewanfires_postfire)[grep("EAR", names(saskatchewanfires_postfire))] =
   grep("YEAR", names(albertafires1_postfire), value = TRUE)
 
-names(albertafires2_postfire)[grep("NAME", names(albertafires2_postfire))] = 
-  names(saskatchewanfires_postfire)[grep("NAME", names(saskatchewanfires_postfire))] = 
+names(albertafires2_postfire)[grep("NAME", names(albertafires2_postfire))] =
+  names(saskatchewanfires_postfire)[grep("NAME", names(saskatchewanfires_postfire))] =
   grep("NAME", names(albertafires1_postfire), value = TRUE)
 
-names(albertafires2_postfire)[grep("FIRE_CODE", names(albertafires2_postfire))] = 
-  names(albertafires1_postfire)[grep("FIRE_NUM", names(albertafires1_postfire))] = 
+names(albertafires2_postfire)[grep("FIRE_CODE", names(albertafires2_postfire))] =
+  names(albertafires1_postfire)[grep("FIRE_NUM", names(albertafires1_postfire))] =
   grep("FIRE_ID", names(saskatchewanfires_postfire), value = TRUE)
 
-names(albertafires2_postfire)[grep("ELEMENT", names(albertafires2_postfire))] = 
-  names(albertafires1_postfire)[grep("SURVIVAL", names(albertafires1_postfire))] = 
+names(albertafires2_postfire)[grep("ELEMENT", names(albertafires2_postfire))] =
+  names(albertafires1_postfire)[grep("SURVIVAL", names(albertafires1_postfire))] =
   names(saskatchewanfires_postfire)[grep("CLASS", names(saskatchewanfires_postfire))] = "SEV_CLAS"
 
 
 ABSK_firePerims <- rbind(albertafires1_postfire[, "FIRE_NAME"], albertafires2_postfire[, "FIRE_NAME"], saskatchewanfires_postfire[, "FIRE_NAME"])
-ABSK_firePerims <- ABSK_firePerims %>% group_by(FIRE_NAME) %>% 
-  summarise(Group = unique(FIRE_NAME)) %>% 
+ABSK_firePerims <- ABSK_firePerims %>% group_by(FIRE_NAME) %>%
+  summarise(Group = unique(FIRE_NAME)) %>%
   st_cast()
 
 # dev(); plot(ABSK_firePerims)
@@ -94,7 +94,7 @@ CA_admin <- CA_admin[CA_admin$PRENAME %in% c("Alberta", "Saskatchewan"),]
 CA_admin <- sp::spTransform(CA_admin, CRSobj = crs(ABSK_firePerims))
 
 FMAs <- Cache(prepInputs, targetFile = "FMA_Boundary_Updated.shp",
-              url = "https://drive.google.com/file/d/1nTFOcrdMf1hIsxd_yNCSTr8RrYNHHwuc/view?usp=sharing", 
+              url = "https://drive.google.com/file/d/1nTFOcrdMf1hIsxd_yNCSTr8RrYNHHwuc/view?usp=sharing",
               destinationPath = "data/FMA", studyArea = CA_admin, writeCropped = FALSE,
               cacheRepo = "analyses/cache")
 

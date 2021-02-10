a6d46a129f88f7ec0eba34c8b10a7ed90d67f038
CeresBarros <cbarros@mail.ubc.ca>
2021-02-10 12:12:54 -0800

    submodule updates

--- analyses/plot_ESAexperiment.R
+++ analyses/plot_ESAexperiment.R
@@ -16,9 +16,9 @@ severities <- rbindlist(lapply(scens, FUN = function(scen) {
   severities <- rbindlist(lapply(list.files(d, pattern = "severity", full.names = TRUE),
                                  FUN = function(rasfile) {
                                    ras <- raster(rasfile)
-                                   severity <- data.table(sev = getValues(ras), 
+                                   severity <- data.table(sev = getValues(ras),
                                                           year = sub(".*Year", "", sub(".tif", "", rasfile)))
-                                   severity 
+                                   severity
                                  }))
   severities[, scen := scen]
   severities
@@ -28,7 +28,7 @@ severities <- rbindlist(lapply(scens, FUN = function(scen) {
 severities[, sev :=-sev]   ## invert values so that biomass gains become positive
 
 dev(width = 6, height = 5)
-ggplot(data = na.omit(severities)) + 
+ggplot(data = na.omit(severities)) +
   geom_density(aes(x = log(sev+abs(min(sev))), fill = scen), alpha = 0.5) +
   geom_vline(aes(xintercept = log(abs(min(sev))))) +
   scale_fill_discrete(labels = c("vegFB_0" = "No veg. feedback",
@@ -43,7 +43,7 @@ ggplot(data = na.omit(severities)) +
 fireSizes <- rbindlist(lapply(scens, FUN = function(scen) {
   d = file.path(outputsPath, scen)
   rstCurrentBurn <- readRDS(list.files(d, pattern = "rstCurrentBurn", full.names = TRUE))
-  
+
   fireSizes <- rbindlist(lapply(rstCurrentBurn, FUN = function(ras) {
     if(class(ras) == "RasterLayer") {
       fireSizes <- as.data.table(table(getValues(ras), dnn = c("FireID")))
@@ -62,7 +62,7 @@ ggplot(data = fireSizes) +
   scale_fill_discrete(labels = c("vegFB_0" = "No veg. feedback",
                                  "vegFB_1" = "W/ veg. feedback")) +
   theme_bw() + theme(legend.position = "bottom", text = element_text(size = 18)) +
-  labs(x = "log(no. pixels)", y = "Density", 
+  labs(x = "log(no. pixels)", y = "Density",
        fill = "", title = "Fire sizes")
 
 # ggsave("fireSizes.png", device = "png", width = 7.270833, height = 3.750000)
@@ -74,65 +74,65 @@ startBiomass <- lapply(scens, FUN = function(scen) {
     ras <- raster(list.files(d, pattern = "biomass.*Year2.tif", full.names = TRUE))
   })
 names(startBiomass) = scens
-  
-  
+
+
 finalBiomass <- lapply(scens, FUN = function(scen) {
   d = file.path(outputsPath, scen)
   ras <- raster(list.files(d, pattern = "biomass.*Year50.tif", full.names = TRUE))
 })
 names(finalBiomass) = scens
 
-plot1 <- gplot(startBiomass[[1]]) +  
+plot1 <- gplot(startBiomass[[1]]) +
   geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1,
             show.legend = FALSE) +
   scale_fill_gradient2(low = "white", high  = "darkgreen") +
   theme_bw() + theme(legend.position = "bottom", axis.title = element_text(size = 12),
                      axis.text = element_text(size = 6),
                      plot.margin = unit(x = c(0,0.1,0,0), units = "in")) +
-  labs(x = "", y = "Latitude", 
+  labs(x = "", y = "Latitude",
        fill = "Biomass", title = "No  veg. feedback")
 
-plot2 <- gplot(startBiomass[[2]]) +  
-  geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1, 
+plot2 <- gplot(startBiomass[[2]]) +
+  geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1,
               show.legend = FALSE) +
   scale_fill_gradient2(low = "white", high  = "darkgreen") +
   theme_bw() + theme(legend.position = "bottom", axis.title = element_text(size = 12),
                      axis.text = element_text(size = 6),
                      plot.margin = unit(x = c(0,0.1,0,0), units = "in")) +
-  labs(x = "", y = "", 
+  labs(x = "", y = "",
        fill = "Biomass", title = "W/  veg. feedback")
 
-plot3 <- gplot(finalBiomass[[1]]) +  
-  geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1, 
+plot3 <- gplot(finalBiomass[[1]]) +
+  geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1,
               show.legend = FALSE) +
   scale_fill_gradient2(low = "white", high  = "darkgreen") +
   theme_bw() + theme(legend.position = "bottom", axis.title = element_text(size = 12),
                      axis.text = element_text(size = 6),
                      plot.margin = unit(x = c(0,0.1,0,0), units = "in")) +
-  labs(x = "Longitude", y = "Latitude", 
+  labs(x = "Longitude", y = "Latitude",
        fill = "Biomass", title = "")
 
-plot4 <- gplot(finalBiomass[[2]]) +  
+plot4 <- gplot(finalBiomass[[2]]) +
   geom_tile(aes(fill=value, alpha=!is.na(value)), size = 0.1) +
   scale_fill_gradient2(low = "white", high  = "darkgreen") +
   scale_alpha_discrete(guide = FALSE) +
   theme_bw() + theme(legend.position = "none", axis.title = element_text(size = 12),
                      axis.text = element_text(size = 6),
                      plot.margin = unit(x = c(0,0.1,0,0), units = "in")) +
-  labs(x = "Longitude", y = "", 
+  labs(x = "Longitude", y = "",
        fill = "Biomass", title = "")
 
 leg <- ggpubr::get_legend(plot4)
 
 grid.arrange(plot1, plot2, plot3, plot4)
-png(filename = "analyses/ESA_mapsFig.png",
+png(filename = "analyses/Figs/ESA_mapsFig.png",
        width = 5.9, height = 8, res = 300, units = "in")
 dev.off()
 
 
 dev()
-png(filename = "analyses/ESA_mapsFig.png",
+png(filename = "analyses/Figs/ESA_mapsFig.png",
     width = 5.9, height = 8, res = 300, units = "in")
-plot(stack(startBiomass[[1]], startBiomass[[2]], 
+plot(stack(startBiomass[[1]], startBiomass[[2]],
            finalBiomass[[1]], finalBiomass[[2]]))
 dev.off()

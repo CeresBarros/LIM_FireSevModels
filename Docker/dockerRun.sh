f9c24a4b45b902c297a8518d2be993ff867580bd
CeresBarros <ceres.barros@nrcan-rncan.gc.ca>
2025-07-25 20:37:11 +0000

    working docker run bash

--- Docker/dockerRun.sh
+++ Docker/dockerRun.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 ## Initialise docker container.
 docker run -d \
--it \
--e PASSWORD=!Caribou4 \
--e USER=cbarros
---memory=100g \
---cpus=32 \
--p 8080:8787 \
---name LIM-docker \
---mount type=bind,source=/home/cbarros/LandscapesInMotion,target=/home/cbarros/LandscapesInMotion \
---mount type=bind,source=/mnt/scratch/cbarros/,target=/mnt/scratch/cbarros/ \
-lim-cbarros
+  -it \
+  -e PASSWORD=!Caribou4 \
+  -e USER=cbarros \
+  --memory=100g \
+  --cpus=32 \
+  -p 8080:8787 \
+  --name LIM-docker \
+  --mount type=bind,source=/home/cbarros/LandscapesInMotion,target=/home/cbarros/LandscapesInMotion \
+  --mount type=bind,source=/mnt/scratch/cbarros/,target=/mnt/scratch/cbarros/ \
+  lim-cbarros
